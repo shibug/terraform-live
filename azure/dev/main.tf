cc2132c2f8d95f777050a07084837c23fc52432e
Shibu Gope <shibu.gope@bnsf.com>
2021-05-21 09:46:00 -0500

    changes

--- azure/dev/main.tf
+++ azure/dev/main.tf
@@ -48,12 +48,20 @@ resource "azurerm_virtual_network" "vn" {
   tags = local.common_tags
 }
 
+# -----------------------------
+# SUBNETS
+# -----------------------------
+
 resource "azurerm_subnet" "worker" {
   name                 = "worker-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.0.0.0/24"]
 }
+
+# -----------------------------
+# NETWORK SECURITY GROUPS
+# -----------------------------
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-${var.env}"
@@ -65,6 +73,37 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_subnet_network_security_group_association" "nsga" {
   subnet_id                 = azurerm_subnet.worker.id
   network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# --------------------------------------------------
+# NETWORK SECURITY RULES
+# --------------------------------------------------
+
+# Allow port 443 access from Internet
+resource "azurerm_network_security_rule" "workerInboundInternetAllow" {
+  name                        = "IInternetA"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefix       = "Internet"
+  source_port_range           = "*"
+  destination_address_prefix  = "VirtualNetwork"
+  destination_port_range     = "3389"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# -----------------------------
+# PUBLIC IPS
+# -----------------------------
+
+resource "azurerm_public_ip" "theta-edge" {
+  name                = "ip-theta-edge"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  tags                = local.common_tags
 }
 
 locals {
--- azure/dev/vms.tf
+++ azure/dev/vms.tf
@@ -1,3 +1,7 @@
+# -----------------------------
+# NETWORK INTERFACES
+# -----------------------------
+
 resource "azurerm_network_interface" "theta-edge" {
   name                = "nic-theta-edge"
   location            = azurerm_resource_group.rg.location
@@ -8,25 +12,26 @@ resource "azurerm_network_interface" "theta-edge" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.worker.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.theta-edge.id
   }
 
   tags = local.common_tags
 }
 
-resource "azurerm_linux_virtual_machine" "theta-edge" {
+# -----------------------------
+# VIRTUAL MACHINES
+# -----------------------------
+
+resource "azurerm_windows_virtual_machine" "theta-edge" {
   name                = "vm-theta-edge"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2s"
   admin_username      = "groot"
+  admin_password      = "Jan19uary"
   network_interface_ids = [
     azurerm_network_interface.theta-edge.id,
   ]
-
-  admin_ssh_key {
-    username   = "groot"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   os_disk {
     name                 = "theta-edge-osdisk"
@@ -35,11 +40,42 @@ resource "azurerm_linux_virtual_machine" "theta-edge" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 
   tags = local.common_tags
 }
+
+#resource "azurerm_linux_virtual_machine" "theta-edge" {
+#  name                = "vm-theta-edge"
+#  resource_group_name = azurerm_resource_group.rg.name
+#  location            = azurerm_resource_group.rg.location
+#  size                = "Standard_B2s"
+#  admin_username      = "groot"
+#  network_interface_ids = [
+#    azurerm_network_interface.theta-edge.id,
+#  ]
+#
+#  admin_ssh_key {
+#    username   = "groot"
+#    public_key = file("~/.ssh/id_rsa.pub")
+#  }
+#
+#  os_disk {
+#    name                 = "theta-edge-osdisk"
+#    caching              = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
+#
+#  source_image_reference {
+#    publisher = "Canonical"
+#    offer     = "UbuntuServer"
+#    sku       = "20.04-LTS"
+#    version   = "latest"
+#  }
+#
+#  tags = local.common_tags
+#}
