ad9f97c9a85a0d18ffdc29760204a0c5487e0f40
Shibu Gope <shibug@gmail.com>
2021-12-22 11:03:05 -0600

    changes

--- azure/dev/scripts/cardano/rotate-keys.sh
+++ azure/dev/scripts/cardano/rotate-keys.sh
@@ -2,9 +2,9 @@
 # BLOCK PRODUCER NODE
 #----------------------------------------------------------------------------------
 dke cardano-bp bash
-cd ${NODE_HOME}/priv
 export CARDANO_NODE_SOCKET_PATH=/opt/cardano/cnode/sockets/node0.socket
 export NODE_HOME=/opt/cardano/cnode
+cd ${NODE_HOME}/priv
 
 slotNo=$(cardano-cli query tip --mainnet | jq -r '.slot')
 slotsPerKESPeriod=$(cat ${NODE_HOME}/files/shelley-genesis.json | jq -r '.slotsPerKESPeriod')
@@ -17,4 +17,8 @@ cardano-cli node key-gen-KES --verification-key-file kes.vkey --signing-key-file
 #----------------------------------------------------------------------------------
 # AIR-GAPPED NODE
 #----------------------------------------------------------------------------------
-dki -v $PWD:/keys --entrypoint cardano-cli shibug/cardano-node:1.31.0 node issue-op-cert --kes-verification-key-file /keys/kes.vkey --cold-signing-key-file /keys/node.skey --operational-certificate-issue-counter /keys/node.counter --kes-period 319 --out-file /keys/op.cert
+cd /Users/shibug/Dropbox/keys/cardano/mainnet
+cp op.cert hot.skey kes.vkey backup/
+scp bp.cardano.mylo.farm:/data/cardano/priv/kes.vkey .
+scp bp.cardano.mylo.farm:/data/cardano/priv/hot.skey .
+dki -v $PWD:/keys --entrypoint cardano-cli shibug/cardano-node:1.31.0-a node issue-op-cert --kes-verification-key-file /keys/kes.vkey --cold-signing-key-file /keys/node.skey --operational-certificate-issue-counter /keys/node.counter --kes-period 373 --out-file /keys/op.cert
